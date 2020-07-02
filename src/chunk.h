1d34e2d1564cf2357701ef58224dfdab20471226
Vijaya Raghavan M <mvijayaraghavan29@gmail.com>
2020-07-02 06:43:19 +0530

    Created a basic VM for running the Bytecode

--- src/chunk.h
+++ src/chunk.h
@@ -1,9 +1,10 @@
 #ifndef clox_chunk_h
 #define clox_chunk_h
-
+#include "value.h"
 #include "common.h"
 
 typedef enum{
+    OP_CONSTANT,
     OP_RETURN,
 } OpCode;
 
@@ -11,9 +12,12 @@ typedef struct {
     int count;
     int capacity;
     uint8_t* code;
+    int* lines;
+    ValueArray constants;
 } Chunk;
 
 void initChunk(Chunk* chunk);
 void freeChunk(Chunk* chunk);
-void writeChunk(Chunk* chunk, uint8_t byte);
+void writeChunk(Chunk* chunk, uint8_t byte, int line);
+int addConstant(Chunk* chunk, Value value);
 #endif
--- src/main.c
+++ src/main.c
@@ -1,14 +1,19 @@
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(int argc, const char *argv[]){
+    initVM();
     Chunk chunk;
     initChunk(&chunk);
-    writeChunk(&chunk, OP_RETURN);
-    writeChunk(&chunk, OP_RETURN);
-    writeChunk(&chunk, OP_RETURN);
-    disassembleChunk(&chunk, "test chunk");
+    int constant = addConstant(&chunk, 1.2);
+    writeChunk(&chunk, OP_CONSTANT, 10);
+    writeChunk(&chunk, constant, 10);
+    writeChunk(&chunk, OP_RETURN, 10);
+    // disassembleChunk(&chunk, "test chunk");
+    interpret(&chunk);
+    freeVM();
     freeChunk(&chunk);
     return 0;
 }
